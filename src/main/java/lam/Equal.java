9f1c997e8f86818be2c4aa18e17706ea5024ab70
Brian McKenna <brian@brianmckenna.org>
2021-04-30 06:05:30 +0000

    Add contramap to Equal

--- src/main/java/lam/Equal.java
+++ src/main/java/lam/Equal.java
@@ -1,5 +1,6 @@
 package lam;
 
+import java.util.function.Function;
 import java.util.function.BiFunction;
 
 public class Equal<A> {
@@ -11,6 +12,10 @@ public class Equal<A> {
 
     public boolean isEqual(final A a, final A b) {
         return f.apply(a, b);
+    }
+
+    public <B> Equal<B> contramap(final Function<B, A> f) {
+        return new Equal<B>((a, b) -> isEqual(f.apply(a), f.apply(b)));
     }
 
     public static Equal<Unit> unitEqual = new Equal<>((a, b) -> true);
