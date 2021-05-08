4303d60eb536cd1acce9c1e9bb211b99b200cc87
Brian McKenna <brian@brianmckenna.org>
2021-05-08 08:34:56 +0000

    Don't rely on NIX_PATH

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
-with import <nixpkgs> { };
-
-runCommand "lam" {
+let
+  pkgs =
+    import (fetchTarball https://channels.nixos.org/nixos-20.09/nixexprs.tar.xz) { };
+in
+pkgs.runCommand "lam" {
   src = ./.;
   buildInputs = [
-    jdk8_headless
-    (haskellPackages.ghcWithPackages (p: [
-      (haskell.lib.appendPatch p.language-java (fetchpatch {
+    pkgs.jdk8_headless
+    (pkgs.haskellPackages.ghcWithPackages (p: [
+      (pkgs.haskell.lib.appendPatch p.language-java (pkgs.fetchpatch {
         url = "https://github.com/vincenthz/language-java/commit/4a52a781ddb9c66b8b66d0960e1390eaa74e5e49.patch";
         sha256 = "0bcf48f9riyy9r09c6fccqjl817paxc6zwiwmhzykira7kc71zrq";
       }))
@@ -18,7 +20,7 @@ runCommand "lam" {
   chmod -R u+w src
   cd src
   export LANG=en_US.UTF-8
-  export LOCALE_ARCHIVE=${glibcLocales}/lib/locale/locale-archive
+  export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
   runhaskell Gen.hs
   mv build/lam.jar $out
 "
